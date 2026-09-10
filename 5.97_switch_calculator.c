988320c804fe1b8a9ee734876eabd1345e0ee287
BADHON <physics1233217001@gmail.com>
2026-09-10 11:48:18 +0600

    Fix switch calculator program to correctly read inputs and display results for all operations

--- 5.97_switch_calculator.c
+++ 5.97_switch_calculator.c
@@ -2,7 +2,20 @@
 
 int main() {
 double num1 , num2;
-printf{"Enter Two number"};
-scanf("%d",&num1);
+char op;
+printf("Enter Two number");
+scanf("%lf",&num1);
+scanf("%lf",&num2);
+printf("Enter the operator (+,-,*,/)");
+scanf(" %c",&op);
+switch(op) {
+    case '+' : {printf("Sum = %lf", num1+num2);}
+
+    case '-' : {printf("Difference = %lf", num1-num2);}
+    case '*' : {printf("Product = %lf", num1*num2);}
+    case '/' : {printf("Quotient = %lf", num1/num2);}
+}
+
+
     return 0;
 }
