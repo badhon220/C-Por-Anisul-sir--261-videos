cbb299b94d0aa8bc5b4973b249915a83d62b06e4
BADHON <physics1233217001@gmail.com>
2026-09-25 00:33:47 +0600

    Remove obsolete pattern files and add new pattern generation programs

---
+++ 5.150_pattrn_part-6_type-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+int n,cul,row;
+printf("Enter number");
+scanf("%d",&n);
+for(row=1;row<=n;row++){
+ for(cul=1;cul<=n-row;cul++){
+    printf(" ");
+ }
+ for(cul=1;cul<=row;cul++){
+    printf("%d",row);
+ }
+ printf("\n");
+}
+    return 0;
+}
