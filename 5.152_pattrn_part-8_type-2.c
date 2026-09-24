fc2db0a86eb2b24f7628642709a0e86658bf74a6
BADHON <physics1233217001@gmail.com>
2026-09-25 01:49:47 +0600

    Add new pattern generation programs for various shapes and sequences

---
+++ 5.152_pattrn_part-8_type-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+int n,cul,row;
+printf("Enter Number");
+scanf("%d",&n);
+for(row= n;row>=1;row--){
+    for(cul=1;cul<=n-row;cul++){
+        printf(" ");
+    }
+     for(cul=1;cul<=row;cul++){
+        printf("%d",row);
+    }
+    printf("\n");
+
+}
+    return 0;
+}
---
+++ 5.153_pattrn_part-9_type-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+int n,cul,row;
+printf("Enter Number");
+scanf("%d",&n);
+for(row=1; row<=n;row++){
+    for(cul=1;cul<=n-row; cul++){
+        printf(" ");
+    }
+    for(cul=1;cul<=row; cul++){
+        printf("%d",cul);
+    }
+    printf("\n");
+}
+for(row=n-1; row>=1;row--){
+    for(cul=1;cul<=n-row; cul++){
+        printf(" ");
+    }
+    for(cul=1;cul<=row; cul++){
+        printf("%d",cul);
+    }
+    printf("\n");
+}
+    return 0;
+}
