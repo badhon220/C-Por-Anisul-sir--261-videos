97ba21ab401516c5a87425e19a30adeb957c283c
BADHON <physics1233217001@gmail.com>
2026-09-25 21:40:10 +0600

    update_prime

--- 5.113.prime.c
+++ 5.113.prime.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main() {
-int n,i;
-printf("Enter number ");
+int i,n,count=0;
+printf("Enter any Number: ");
 scanf("%d",&n);
+if(n==0 || n==1)
+count=1; 
 for(i=2;i<n;i++){
-    if(n%i==0){
-        printf("Not prime");
-        break;
-    }
-    printf("Prime");
-    break;
+    if(n%i==0)
+    count=1;
 }
+if(count==0)
+printf("the number is prime number");
+else
+printf("the number is not prime number");
     return 0;
 }
